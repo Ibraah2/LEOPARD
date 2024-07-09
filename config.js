7c5a199aed8c9576ba335bef54589945798d2e2b
Ibraah2 <yohmkuu3148@gmail.com>
2024-07-09 12:09:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,78 +1,29 @@
-//---> END DECOR
-           
-           
-           
 
+const fs = require('fs')
+const chalk = require('chalk')
 
+// ganti info bot dibawah ini
+global.botName = "LEOPARD"
+global.ownerName = "IBRAAH"
+global.botNumber = "254702302770"
+global.devNumber = ["254702302770"]
 
+global.Auto_Typing = true // auto typing
+global.Auto_Recording = true // auto recording
+global.Auto_ReadPesan = false // auto read messages
 
+global.domain = "https://example.id" // Ganti Domain Lu
+global.key_plta = "-" // Isi Apikey Plta Lu
+global.key_pltc = "-" // Isi Apikey Pltc Lu
+global.nama_host = "LECCYOFC" // Ganti Nama Store Atau nama Host Lu
 
-
-
-  
-
-
-
-
-
-          //---> SEMANGAT SCROLNYA BROK WKWK
-          
-          
-          //---> CREATED BY PUTZXZMOODZ
-          
-          
-          
-          //---> JANGAN LUPA SUBSCRIBE
-          
-          
-          
-          //---> JANGAN LUPA LIKE & COMENT
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
 
 
 
